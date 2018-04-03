02e3e1601da6c253e46a45201c18962d04ce3708
李传晓 <chuanxiao.li@jieshunpay.cnn>
2018-04-03 10:58:03 +0800

    jjj

--- src/politie/jenkins/JenkinsProjectList.groovy
+++ src/politie/jenkins/JenkinsProjectList.groovy
@@ -3,9 +3,11 @@ package politie.jenkins
 def projectlist = ["appfront","member","tran","oms"]
 
 def project(){
+    def projectname
     for(projectname in projectlist) {
         println projectname
     }
+    return projectlist
 }
 
-return this
+//return this
