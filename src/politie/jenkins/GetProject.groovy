367aab35622fff43c43ad331044a68b5902f4dc7
李传晓 <chuanxiao.li@jieshunpay.cnn>
2018-04-04 14:33:23 +0800

    hehe

--- src/politie/jenkins/GetProject.groovy
+++ src/politie/jenkins/GetProject.groovy
@@ -5,15 +5,7 @@ import java.io.File
 import static groovy.io.FileType.*
 
 def getProject(BASEDIR){
-    FILEPATH = "/var/jenkins_home/jobs/jst-tran-Analysis/builds/"+BASEDIR+"/libs/pipeline-library/src/politie/jenkins/JenkinsProjectList.groovy"
-
-    Constants.PROJECT_LIST = new File(FILEPATH).readLines()
-   
-}
-
-
-def props = new Properties()
-new File("message.properties").withInputStream { 
-  stream -> props.load(stream) 
-}
+    FILEPATH = BASEDIR+"/libs/pipeline-library/src/politie/jenkins/JenkinsProjectList.groovy"
+    Constants.PROJECT_LIST = new File(FILEPATH).readLines() 
+    }   
 return this;
--- src/politie/jenkins/JenkinsPipelineBuilder.groovy
+++ src/politie/jenkins/JenkinsPipelineBuilder.groovy
@@ -24,7 +24,7 @@ void allbuild(){
         sh "echo $JENKINS_HOME"
         sh "env"
        // BASEDIR = env.WORKSPACE+"/"+env.BUILD_ID
-       BASEDIR = env.BUILD_ID
+       BASEDIR = env.JENKINS_HOME+"/jobs/"+env.JOB_NAME+"/builds/"+env.BUILD_ID
         projectList.getProject(BASEDIR)
     }  
     for(project in Constants.PROJECT_LIST) {
