0bbd91783d0ad8d4f196f8838f2bedd577fa38d9
李传晓 <chuanxiao.li@jieshunpay.cnn>
2018-04-04 10:44:40 +0800

    hehe

--- src/politie/jenkins/GetProject.groovy
+++ src/politie/jenkins/GetProject.groovy
@@ -6,9 +6,14 @@ import java.io.File
 def getProject(){
     path = new  File(".").getPath()
     println path
-    new File("src/politie/jenkins/JenkinsProjectList.groovy").eachLine { line ->Constants.PROJECT_LIST.add($line)
+    new File(baseDir,"JenkinsProjectList.groovy").eachLine { line ->Constants.PROJECT_LIST.add($line)
         
     }
 }
 
+
+def props = new Properties()
+new File("message.properties").withInputStream { 
+  stream -> props.load(stream) 
+}
 return this;
