7b6b0a0d2fdd04ada8e7e98ee413e2ce1a1753db
李传晓 <chuanxiao.li@jieshunpay.cnn>
2018-04-04 10:11:01 +0800

    hehe

--- src/politie/jenkins/GetProject.groovy
+++ src/politie/jenkins/GetProject.groovy
@@ -4,6 +4,7 @@ import politie.jenkins.Constants
 import java.io.File
 
 def getProject(){
+    new  File(".").getPath()
     new File("politie/jenkins/JenkinsProjectList.groovy").eachLine { line ->Constants.PROJECT_LIST.add($line)
         
     }
