e2f07e4dc09fbd4d842336f150715235e5826d79
李传晓 <chuanxiao.li@jieshunpay.cnn>
2018-04-08 16:36:06 +0800

    hh

--- src/politie/jenkins/CreateReport.groovy
+++ src/politie/jenkins/CreateReport.groovy
@@ -10,10 +10,11 @@ import java.io.*
 void createreport(failed_projects,BASEDIR){
 
 templatepath = BASEDIR+"/libs/pipeline-library/src/politie/jenkins/mailtemplate.groovy"
+reportpath = BASEDIR+"/libs/pipeline-library/src/politie/jenkins/report.html"
 def file = new File(templatepath)
 
 def writable = new SimpleTemplateEngine().createTemplate(file).make(['failed_projects':failed_projects])
-def destination = new FileWriter('report.html')
+def destination = new FileWriter(reportpath)
 writable.writeTo(destination)
 destination.flush()
 destination.close()
